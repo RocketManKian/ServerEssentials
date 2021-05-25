c7808472b47cb9cab551bacacaad97c1c610e1bc
RocketManKian <kicod311@gmail.com>
2021-05-26 00:59:55 +0100

    Version 1.60.0

--- src/main/java/me/rocketmankianproductions/serveressentials/events/PlayerChatEvent.java
+++ src/main/java/me/rocketmankianproductions/serveressentials/events/PlayerChatEvent.java
@@ -8,12 +8,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatEvent implements Listener {
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST)
     public void onChat(AsyncPlayerChatEvent c) {
         Player player = c.getPlayer();
         if (StaffChat.staffchat.contains(player)) {
