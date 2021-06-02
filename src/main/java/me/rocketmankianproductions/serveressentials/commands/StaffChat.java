87d29f88b39b171dfbac55b222fc5811e58a6091
RocketManKian <kicod311@gmail.com>
2021-06-02 18:50:19 +0100

    Version 1.60.3

--- src/main/java/me/rocketmankianproductions/serveressentials/commands/StaffChat.java
+++ src/main/java/me/rocketmankianproductions/serveressentials/commands/StaffChat.java
@@ -1,5 +1,9 @@
 package me.rocketmankianproductions.serveressentials.commands;
 
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
+import me.rocketmankianproductions.serveressentials.ServerEssentials;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -26,6 +30,18 @@ public class StaffChat implements CommandExecutor {
                     }else{
                         player.sendMessage(ChatColor.GOLD + "StaffChat has been" + ChatColor.RED + " disabled!");
                         staffchat.remove(player);
+                        return true;
+                    }
+                }else if (args.length >= 1){
+                    String myArgs = String.join(" ", args);
+                    String playername = ChatColor.stripColor(player.getDisplayName());
+                    TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName("staff-chat");
+                    if (textChannel != null && ServerEssentials.isConnectedToDiscordSRV && ServerEssentials.plugin.getConfig().getBoolean("enable-discord-integration")){
+                        textChannel.sendMessage("**" + playername + "** » " + myArgs).queue();
+                        Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + playername + ": " + ChatColor.GRAY + myArgs, "se.staffchat");
+                        return true;
+                    }else{
+                        Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + playername + ": " + ChatColor.GRAY + myArgs, "se.staffchat");
                         return true;
                     }
                 }
