a6fc2132aae10fa6b0f9ed047add1d01a705a232
RocketManKian <kicod311@gmail.com>
2022-01-23 02:54:25 +0000

    Version 1.76.1
    Fixed an issue which made it so that StaffChat messages wouldn't send if Discord Integration was enabled and the Channel was null.
    Added a new config option, 'enable-bungee-integration'. This option will allow you to toggle Bungeecord Integration on and off for the StaffChat command.
    Added full support for Bungeecord Servers for the StaffChat command. When setting the new config option to 'true' you will now have the StaffChat message send through all Servers, rather than just the Server it was sent on as it was previously. Additionally, there will also be a Prefix to the Message saying which Server it was sent from.

--- src/main/java/me/rocketmankianproductions/serveressentials/events/PlayerChatEvent.java
+++ src/main/java/me/rocketmankianproductions/serveressentials/events/PlayerChatEvent.java
@@ -21,19 +21,45 @@ public class PlayerChatEvent implements Listener {
         Player player = c.getPlayer();
         if (StaffChat.staffchat.contains(player)) {
             String scmessage = c.getMessage();
-            if (ServerEssentials.isConnectedToDiscordSRV && ServerEssentials.getPlugin().getConfig().getBoolean("enable-discord-integration") == true) {
-                String channel = ServerEssentials.getPlugin().getConfig().getString("staff-chat-channel-name");
-                TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channel);
-                if (textChannel != null) {
-                    String player1 = player.getDisplayName();
-                    player1 = ChatColor.stripColor(player1);
-                    textChannel.sendMessage("**" + player1 + "** » " + scmessage).queue();
+            if (ServerEssentials.isBungeeCordEnabled){
+                if (ServerEssentials.isConnectedToDiscordSRV && ServerEssentials.getPlugin().getConfig().getBoolean("enable-discord-integration") == true) {
+                    String channel = ServerEssentials.getPlugin().getConfig().getString("staff-chat-channel-name");
+                    TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channel);
+                    if (textChannel != null) {
+                        String player1 = player.getDisplayName();
+                        player1 = ChatColor.stripColor(player1);
+                        textChannel.sendMessage("**" + player1 + "** » " + scmessage).queue();
+                        StaffChat.msg = scmessage;
+                        StaffChat.getServer();
+                        c.setCancelled(true);
+                    }else{
+                        StaffChat.msg = scmessage;
+                        StaffChat.getServer();
+                        c.setCancelled(true);
+                    }
+                } else {
+                    StaffChat.msg = scmessage;
+                    StaffChat.getServer();
+                    c.setCancelled(true);
+                }
+            }else{
+                if (ServerEssentials.isConnectedToDiscordSRV && ServerEssentials.getPlugin().getConfig().getBoolean("enable-discord-integration") == true) {
+                    String channel = ServerEssentials.getPlugin().getConfig().getString("staff-chat-channel-name");
+                    TextChannel textChannel = DiscordSRV.getPlugin().getDestinationTextChannelForGameChannelName(channel);
+                    if (textChannel != null) {
+                        String player1 = player.getDisplayName();
+                        player1 = ChatColor.stripColor(player1);
+                        textChannel.sendMessage("**" + player1 + "** » " + scmessage).queue();
+                        Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + player.getName() + ": " + ChatColor.GRAY + scmessage, "se.staffchat");
+                        c.setCancelled(true);
+                    }else{
+                        Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + player.getName() + ": " + ChatColor.GRAY + scmessage, "se.staffchat");
+                        c.setCancelled(true);
+                    }
+                } else {
                     Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + player.getName() + ": " + ChatColor.GRAY + scmessage, "se.staffchat");
                     c.setCancelled(true);
                 }
-            } else {
-                Bukkit.broadcast(ChatColor.translateAlternateColorCodes('&', "&d(&5&lStaff&d) ") + ChatColor.LIGHT_PURPLE + player.getName() + ": " + ChatColor.GRAY + scmessage, "se.staffchat");
-                c.setCancelled(true);
             }
         }
         if (ServerEssentials.plugin.getConfig().getBoolean("chat-features")){
