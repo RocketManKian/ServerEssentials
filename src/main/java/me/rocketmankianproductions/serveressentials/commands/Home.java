08f3fd3b36ff20ebe54d6f5237bef0ee74b8810f
RocketManKian <kicod311@gmail.com>
2022-04-08 01:43:06 +0100

    Version 1.77.2
    Fixed an issue which prevented you from being teleported to your home if you only had multiple homes set and you had 'home-movement-cancel' set to false in the config.yml file.

--- src/main/java/me/rocketmankianproductions/serveressentials/commands/Home.java
+++ src/main/java/me/rocketmankianproductions/serveressentials/commands/Home.java
@@ -125,38 +125,36 @@ public class Home implements CommandExecutor {
                                     }
                                     hometeleport.put(player.getUniqueId(), Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((ServerEssentials.plugin), new Runnable() {
                                         public void run() {
-                                            if (cancel.contains(player.getUniqueId())){
-                                                if (hometeleport.containsKey(player.getUniqueId())) {
-                                                    if (ServerEssentials.plugin.getConfig().getBoolean("home-save")) {
-                                                        if (Back.location.containsKey(player.getUniqueId())) {
-                                                            Back.location.remove(player.getUniqueId());
-                                                            Back.location.put(player.getUniqueId(), player.getLocation());
-                                                        } else {
-                                                            Back.location.put(player.getUniqueId(), player.getLocation());
-                                                        }
-                                                    } else if (player.hasPermission("se.back.bypass")) {
-                                                        if (Back.location.containsKey(player.getUniqueId())) {
-                                                            Back.location.remove(player.getUniqueId());
-                                                            Back.location.put(player.getUniqueId(), player.getLocation());
-                                                        } else {
-                                                            Back.location.put(player.getUniqueId(), player.getLocation());
-                                                        }
+                                            if (hometeleport.containsKey(player.getUniqueId())) {
+                                                if (ServerEssentials.plugin.getConfig().getBoolean("home-save")) {
+                                                    if (Back.location.containsKey(player.getUniqueId())) {
+                                                        Back.location.remove(player.getUniqueId());
+                                                        Back.location.put(player.getUniqueId(), player.getLocation());
+                                                    } else {
+                                                        Back.location.put(player.getUniqueId(), player.getLocation());
                                                     }
-                                                    if (loc.isWorldLoaded()){
-                                                        // Teleporting Player
-                                                        player.teleport(loc);
-                                                        Boolean subtitle = ServerEssentials.plugin.getConfig().getBoolean("enable-home-subtitle");
-                                                        if (subtitle) {
-                                                            String msg = Lang.fileConfig.getString("home-subtitle").replace("<home>", args[0]);
-                                                            player.sendTitle(ChatColor.translateAlternateColorCodes('&', msg), null);
-                                                        } else {
-                                                            String msg = Lang.fileConfig.getString("home-message").replace("<home>", args[0]);
-                                                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                                                        }
-                                                    }else{
-                                                        String msg = Lang.fileConfig.getString("home-world-invalid");
-                                                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                                                } else if (player.hasPermission("se.back.bypass")) {
+                                                    if (Back.location.containsKey(player.getUniqueId())) {
+                                                        Back.location.remove(player.getUniqueId());
+                                                        Back.location.put(player.getUniqueId(), player.getLocation());
+                                                    } else {
+                                                        Back.location.put(player.getUniqueId(), player.getLocation());
                                                     }
+                                                }
+                                                if (loc.isWorldLoaded()) {
+                                                    // Teleporting Player
+                                                    player.teleport(loc);
+                                                    Boolean subtitle = ServerEssentials.plugin.getConfig().getBoolean("enable-home-subtitle");
+                                                    if (subtitle) {
+                                                        String msg = Lang.fileConfig.getString("home-subtitle").replace("<home>", args[0]);
+                                                        player.sendTitle(ChatColor.translateAlternateColorCodes('&', msg), null);
+                                                    } else {
+                                                        String msg = Lang.fileConfig.getString("home-message").replace("<home>", args[0]);
+                                                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                                                    }
+                                                } else {
+                                                    String msg = Lang.fileConfig.getString("home-world-invalid");
+                                                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
                                                 }
                                             }
                                         }
