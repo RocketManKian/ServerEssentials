aac5b2467015f6a04e91a53c1d05b9824f2ce563
RocketManKian <kicod311@gmail.com>
2022-07-17 15:19:21 +0100

    Version 1.80.0
    Added a brand new command which is '/sudo' and allows you to execute any command in the game as another player. For example... '/sudo RocketManKian /msg TrappedHeart hi' will send a message to TrappedHeart from the RocketManKian account. This was requested over on the spigot page, so enjoy!
    Added two new Lang.yml File Values which are: 'sudo-successful' and 'sudo-command-invalid' both of which are used for the new Sudo Command.
    Added two more brand new commands which are: '/youtube' and '/twitch'. These commands are completely configurable with Placeholders and text via the two new Lang.yml file values that are: 'youtube-command' and 'twitch-command'. This was suggested over on the SE Support Discord, so make sure to join if you haven't already!
    Created a brand new Method in the Fly Class which eliminates almost all of the duplicate code present.
    Fixed an internal error that could occur with the '/fly' command as there was previously no 'if target == null' check, that is no longer the case and it will now send the 'target-offline' lang.yml value.

--- src/main/java/me/rocketmankianproductions/serveressentials/commands/Fly.java
+++ src/main/java/me/rocketmankianproductions/serveressentials/commands/Fly.java
@@ -4,9 +4,7 @@ import me.rocketmankianproductions.serveressentials.ServerEssentials;
 import me.rocketmankianproductions.serveressentials.file.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,66 +15,48 @@ public class Fly implements CommandExecutor {
             Player player = (Player) sender;
             if (ServerEssentials.permissionChecker(player, "se.fly")) {
                 if (args.length == 0) {
-                    if (player.getAllowFlight() == true) {
-                        player.setFlying(true);
-                        String msg = Lang.fileConfig.getString("fly-disabled");
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                        player.setAllowFlight(false);
-                        return true;
-                    } else {
-                        player.setFlying(false);
-                        String msg = Lang.fileConfig.getString("fly-enabled");
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                        player.setAllowFlight(true);
-                        return true;
-                    }
+                    flyToggle(player, false, null);
                 } else if (args.length == 1) {
                     Player targetPlayer = Bukkit.getServer().getPlayer(args[0]);
-                    if (targetPlayer.getAllowFlight() == true) {
-                        targetPlayer.setFlying(true);
-                        String msg = Lang.fileConfig.getString("fly-disabled");
-                        targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                        String msg2 = Lang.fileConfig.getString("fly-target-disabled").replace("<target>", targetPlayer.getName());
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
-                        targetPlayer.setAllowFlight(false);
-                        return true;
-                    } else {
-                        targetPlayer.setFlying(false);
-                        String msg = Lang.fileConfig.getString("fly-enabled");
-                        targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                        String msg2 = Lang.fileConfig.getString("fly-target-enabled").replace("<target>", targetPlayer.getName());
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
-                        targetPlayer.setAllowFlight(true);
-                        return true;
-                    }
+                    flyToggle(targetPlayer, true, player);
                 } else {
                     String msg = Lang.fileConfig.getString("incorrect-format").replace("<command>", "/fly (player)");
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
                     return true;
                 }
             }
-        } else {
+        } else if (sender instanceof ConsoleCommandSender || sender instanceof BlockCommandSender){
             if (args.length == 1) {
                 Player targetPlayer = Bukkit.getServer().getPlayer(args[0]);
-                if (targetPlayer.getAllowFlight() == true) {
-                    targetPlayer.setFlying(true);
-                    String msg = Lang.fileConfig.getString("fly-disabled");
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                    String msg2 = Lang.fileConfig.getString("fly-target-disabled").replace("<target>", targetPlayer.getName());
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
-                    targetPlayer.setAllowFlight(false);
-                    return true;
-                } else {
-                    targetPlayer.setFlying(false);
-                    String msg = Lang.fileConfig.getString("fly-enabled");
-                    targetPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
-                    String msg2 = Lang.fileConfig.getString("fly-target-enabled").replace("<target>", targetPlayer.getName());
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
-                    targetPlayer.setAllowFlight(true);
-                    return true;
-                }
+                flyToggle(targetPlayer, true, sender);
             }
         }
         return false;
+    }
+    public void flyToggle(Player player, boolean target, CommandSender sender){
+        if (player != null){
+            if (player.getAllowFlight() == true) {
+                player.setFlying(true);
+                String msg = Lang.fileConfig.getString("fly-disabled");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                if (target){
+                    String msg2 = Lang.fileConfig.getString("fly-target-disabled").replace("<target>", player.getName());
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
+                }
+                player.setAllowFlight(false);
+            } else {
+                player.setFlying(false);
+                String msg = Lang.fileConfig.getString("fly-enabled");
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                player.setAllowFlight(true);
+                if (target){
+                    String msg2 = Lang.fileConfig.getString("fly-target-enabled").replace("<target>", player.getName());
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg2));
+                }
+            }
+        }else{
+            String msg = Lang.fileConfig.getString("target-offline");
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+        }
     }
 }
