17841ea406cde200b561d61c0035e75ac9ec7447
RocketManKian <kicod311@gmail.com>
2022-08-02 22:32:18 +0100

    Version 1.80.7
    Fixed a typo in the config.yml file.
    Made it so the 'AcceptTheRules' plugin now loads before Server Essentials, this will make it so AcceptTheRules takes priority over the '/rules' command.
    Added a brand new command which is '/ping' and will allow you to display the Ping of yourself, or the targeted player. This command is also executable in Console.
    Added a new permission which is 'se.ping' and is required to use the '/ping' command.
    Added two new Lang.yml file values which are: 'ping-self' and 'ping-target' and are used as the primary messages for the Ping command.

---
+++ src/main/java/me/rocketmankianproductions/serveressentials/commands/Ping.java
@@ -0,0 +1,63 @@
+package me.rocketmankianproductions.serveressentials.commands;
+
+import me.rocketmankianproductions.serveressentials.ServerEssentials;
+import me.rocketmankianproductions.serveressentials.file.Lang;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class Ping implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            if (ServerEssentials.permissionChecker(player, "se.ping")){
+                if (args.length == 0){
+                    String msg = Lang.fileConfig.getString("ping-self").replace("<ping>", String.valueOf(player.getPing()));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                    return true;
+                }else if (args.length == 1){
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target == null){
+                        String msg = Lang.fileConfig.getString("target-offline");
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                        return true;
+                    }else{
+                        String msg = Lang.fileConfig.getString("ping-self").replace("<target>", target.getName()).replace("<ping>", String.valueOf(target.getPing()));
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                        return true;
+                    }
+                }else{
+                    String msg = Lang.fileConfig.getString("incorrect-format").replace("<command>", "/ping");
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                    return true;
+                }
+            }
+        }else if (sender instanceof ConsoleCommandSender){
+            ConsoleCommandSender console = Bukkit.getConsoleSender();
+            if (args.length == 1){
+                Player target = Bukkit.getPlayer(args[0]);
+                if (target == null){
+                    String msg = Lang.fileConfig.getString("target-offline");
+                    console.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                    return true;
+                }else{
+                    String msg = Lang.fileConfig.getString("ping-self").replace("<target>", target.getName()).replace("<ping>", String.valueOf(target.getPing()));
+                    console.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                    return true;
+                }
+            }else{
+                String msg = Lang.fileConfig.getString("incorrect-format").replace("<command>", "/ping <target>");
+                console.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+                return true;
+            }
+        }
+        return false;
+    }
+}
