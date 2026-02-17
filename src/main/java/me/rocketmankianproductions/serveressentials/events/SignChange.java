7b9c8298c0b39d82587201c6b0513762f97c52c1
RocketManKian <kicod311@gmail.com>
2026-02-17 19:31:11 +0000

    2.3.5 Added the ability to use colour codes on signs if you have the following new permission: se.sign.colour.

---
+++ src/main/java/me/rocketmankianproductions/serveressentials/events/SignChange.java
@@ -0,0 +1,21 @@
+package me.rocketmankianproductions.serveressentials.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class SignChange implements Listener {
+
+    @EventHandler
+    public void signChangeEvent(SignChangeEvent e) {
+        Player player = e.getPlayer();
+        if (player.hasPermission("se.sign.colour")) {
+            e.setLine(0, ChatColor.translateAlternateColorCodes('&', e.getLine(0)));
+            e.setLine(1, ChatColor.translateAlternateColorCodes('&', e.getLine(1)));
+            e.setLine(2, ChatColor.translateAlternateColorCodes('&', e.getLine(2)));
+            e.setLine(3, ChatColor.translateAlternateColorCodes('&', e.getLine(3)));
+        }
+    }
+}
